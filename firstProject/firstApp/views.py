f9e6a1bf9e02e74b2c9cff49e4d8cc97c4aa5d09
amitbhalla <amit9815@gmail.com>
2021-08-10 23:32:15 +0530

    first commit

---
+++ firstProject/firstApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Create your views here.
+def employeeView(request):
+    emp = {
+        'id': 123,
+        'name': 'John',
+        'salary': 100000,
+    }
+    return JsonResponse(emp)
